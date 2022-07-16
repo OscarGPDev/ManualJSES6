299b6fdf425e8750d8e48414f450ca4eac07a81b
Oscar Guzman Perez <oscarprograb@gmail.com>
2022-07-16 14:02:45 -0500

    DOM API, se agregaron funciones adicionales

--- 8 Web API/8.1 DOM/DOM API.js
+++ 8 Web API/8.1 DOM/DOM API.js
@@ -1,13 +1,15 @@
-
-
 window.onload = () => {
-    const imagenes = ["https://w.wallhaven.cc/full/l3/wallhaven-l315vy.png"];
+    const imagenes = [
+        "https://w.wallhaven.cc/full/l3/wallhaven-l315vy.png",
+        "https://w.wallhaven.cc/full/j3/wallhaven-j3gz1w.jpg",
+        "https://w.wallhaven.cc/full/72/wallhaven-725mg9.png",
+        "https://w.wallhaven.cc/full/rd/wallhaven-rd83mq.jpg"
+    ];
     const display = document.getElementById("display");
-    const botonSiguiente = document.getElementsByName("siguiente")[0];
-    const botonAnterior = document.getElementsByName("anterior")[0];
-    const botonPantallaCompleta = document.getElementsByName("pantallaCompleta")[0];
-    const botonMostrarMensaje = document.getElementById("mostrarMensaje");
+    const botones = Array.from(document.getElementsByName("boton"));
     const campoMensaje = document.getElementById("mensaje");
+    const mensajes = document.getElementById("mensajes");
+    const colorValor = document.getElementById("colorValor");
 
     let imagenActual = 0;
     const imagenSiguiente = () => {
@@ -18,28 +20,36 @@ window.onload = () => {
         }
 
         display.src = imagenes[imagenActual];
-    }
+    };
     const imagenAnterior = () => {
         if (imagenActual > 0) {
             imagenActual--;
         } else {
             imagenActual = imagenes.length - 1;
         }
-        imagenActual--;
         display.src = imagenes[imagenActual];
-    }
+    };
     const pantallaCompleta = () => {
         display.requestFullscreen();
-    }
+    };
     const mostrarMensaje = () => {
-        Swal.fire(
-            '',
-            campoMensaje.value,
-            'success'
-        )
+        mensajes.innerHTML += `${campoMensaje.value}<br/>`;
+        campoMensaje.value = "";
+    };
+    const cambiarColor = () => {
+        colorValor.click();
+    };
+    const inicializar=()=>{
+        botones.find(boton => boton.id === "siguiente").onclick = imagenSiguiente;
+        botones.find(boton => boton.id === "anterior").onclick = imagenAnterior;
+        botones.find(boton => boton.id === "pantallaCompleta").onclick = pantallaCompleta;
+        botones.find(boton => boton.id === "mostrarMensaje").onclick = mostrarMensaje;
+        botones.find(boton => boton.id === "cambiarColor").onclick = cambiarColor;
+        colorValor.onchange = () => {
+            mensajes.style.color = colorValor.value;
+        };
+        display.src=imagenes[0];
     }
-    botonSiguiente.onclick = imagenSiguiente;
-    botonAnterior.onclick = imagenAnterior;
-    botonPantallaCompleta.onclick = pantallaCompleta;
-    botonMostrarMensaje.onclick = mostrarMensaje;
-}
+    inicializar();
+
+};
