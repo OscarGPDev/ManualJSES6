9078a0a19b3ee03dab48b6f94b1c197cc0ff0c8d
Oscar Guzman <oscarprograb@gmail.com>
2022-08-12 11:59:24 -0500

    Se agregó información de salida

--- 2 Funciones flecha.js
+++ 2 Funciones flecha.js
@@ -5,13 +5,19 @@
 function sumaFuncionNormal(n1, n2) {
     return n1 + n2;
 }
+
+console.log(`sumaFuncionNormal(3, 4): ${sumaFuncionNormal(3, 4)}`);
 // Este es su equivalente como función flecha
 const sumaFuncionFlecha = (n1, n2) => n1 + n2;
+console.log(`sumaFuncionFlecha(3, 4): ${sumaFuncionFlecha(3, 4)}`);
 // También puede escribirse de la siguiente manera
 const sumaFuncionFlecha1 = (n1, n2) => {
     return n1 + n2;
 };
+console.log(`sumaFuncionFlecha1(3, 4): ${sumaFuncionFlecha1(3, 4)}`);
 // Si queremos devolver un objeto en una sola línea con una función flecha debemos envolverlo primero entre paréntesis
-const sumaFuncionFlecha2 = (n1, n2) => ({ resultado: n1 + n2 });
+const sumaFuncionFlecha2 = (n1, n2) => ({resultado: n1 + n2});
+console.log(`sumaFuncionFlecha2(3, 4): ${sumaFuncionFlecha2(3, 4).resultado}`);
 // Cuando la función flecha tiene un solo parámetro, no es necesario envolverlo entre paréntesis.
 const cuadradoFuncionFlecha = n1 => n1 ** 2;
+console.log(`cuadradoFuncionFlecha(3, 2): ${cuadradoFuncionFlecha(3, 2)}`);
--- 6 Desestructuracion.js
+++ 6 Desestructuracion.js
@@ -3,11 +3,14 @@
  */
 // Tenemos el siguiente arreglo, en este caso ordenado
 const arregloOrdenadoMayorAMenor = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+console.log(`arregloOrdenadoMayorAMenor: ${arregloOrdenadoMayorAMenor}`)
 // Supongamos que usara varias veces la primera posición, que consiste en el valor mas grande del arreglo, es conveniente desestructurarlo para tener un nombre más significativo
 const [valorMasGrande] = arregloOrdenadoMayorAMenor;
+console.log(`valorMasGrande: ${valorMasGrande}`);
 // Podemos obtener tantas variables como deseemos, pero solo se puede hacer en orden, es decir, no podemos extraer una posición especifica, siempre empezaremos por la 0
 // Con el patrón rest que se indica con '...nombreDeLaVariable', podemos asignar el resto de valores en otra variable
 const [valorMasGrande1, segundoValorMasGrande, tercerValorMasGrande, ...restoDeValores] = arregloOrdenadoMayorAMenor;
+console.log(`valorMasGrande1, segundoValorMasGrande, tercerValorMasGrande, ...restoDeValores: ${valorMasGrande1}, ${segundoValorMasGrande}, ${tercerValorMasGrande}, ${restoDeValores}`);
 
 // Supongamos que tenemos el siguiente objeto que ejemplifica el resultado de una búsqueda.
 const resultadoDeBusqueda = {
@@ -23,13 +26,20 @@ const resultadoDeBusqueda = {
     total: 7,
     mejorCoincidencia: "resultado 3"
 };
+console.log(`resultadoDeBusqueda: ${resultadoDeBusqueda}`);
 // Supongamos que solo nos interesa la mejor coincidencia, podemos desestructurar un objeto de la siguiente manera.
-const { mejorCoincidencia } = resultadoDeBusqueda;
+const {mejorCoincidencia} = resultadoDeBusqueda;
+console.log(`mejorCoincidencia: ${mejorCoincidencia}`);
 // Ademas, podemos cambiarles el nombre, lo cual puede llegar a ser util para mantener la consistencia en el código, haciendo uso de la siguiente nomenclatura
-const { mejorCoincidencia: nuevoNombre } = resultadoDeBusqueda;
+const {mejorCoincidencia: nuevoNombre} = resultadoDeBusqueda;
+console.log(`nuevoNombre: ${nuevoNombre}`);
 // Otro uso util de la desestructuración es que podemos crear copias, tanto de objetos y arreglos. Spoiler: es muy útil y especialmente necesario en React
-const copiaDelResultadoDeBusqueda = { ...resultadoDeBusqueda };
+const copiaDelResultadoDeBusqueda = {...resultadoDeBusqueda};
+console.log(`copiaDelResultadoDeBusqueda: ${copiaDelResultadoDeBusqueda}`);
 const copiaDeArregloOrdenado = [...arregloOrdenadoMayorAMenor];
+console.log(`copiaDeArregloOrdenado: ${copiaDeArregloOrdenado}`);
 // Al desestructurar podemos agregar información.
-const copiaDelResultadoDeBusquedaModificada = { ...resultadoDeBusqueda, cadenaBuscada: "resultado 3" };
+const copiaDelResultadoDeBusquedaModificada = {...resultadoDeBusqueda, cadenaBuscada: "resultado 3"};
+console.log(`copiaDelResultadoDeBusquedaModificada: ${copiaDelResultadoDeBusquedaModificada}`);
 const copiaDeArregloOrdenadoConNuevoMayor = [11, ...arregloOrdenadoMayorAMenor];
+console.log(`copiaDeArregloOrdenadoConNuevoMayor: ${copiaDeArregloOrdenadoConNuevoMayor}`);
